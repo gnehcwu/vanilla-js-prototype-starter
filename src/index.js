750d6121bc1f5d88b47f300753529574ca3dbb17
Cheng Wu <iwucheng@outlook.com>
2021-04-02 22:40:52 +0100

    Add babel & JS class fields syntax support 🤷‍♂️

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import './assets/styles/blokkfont.css';
 import './assets/styles/custom.css';
 
-console.log('hello vanilla js prototype starter');
-
-function test() {
-  console.log('cheng wu');
-}
-
-test();
+// Your code goes here...
--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,17 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
     ],
   },
 };
