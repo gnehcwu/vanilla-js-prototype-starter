c63ba2449d94bdf2d53f14a03781a2bc7969428e
Cheng Wu <iwucheng@outlook.com>
2021-04-05 13:47:26 +0100

    Remove unnecessary post-css options 🥚

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,9 +34,6 @@ module.exports = merge(common, {
                     'postcss-preset-env',
                     {
                       stage: 0,
-                      features: {
-                        'nesting-rules': true,
-                      },
                     },
                   ],
                 ],
